3e0ef06322e4fb0ff5a73d4e0f0c9b3fa3e865ac
Mahmudul Islam <iamahmudul@gmail.com>
2021-10-13 00:37:05 +0600

    grammar start

---
+++ index.js
@@ -0,0 +1,9 @@
+var hello = 'this is first';
+console.clear();
+console.log(hello);
+var hello1 = 'this is first';
+
+document.writeln('Hello, world');
+document.writeln('<br>');
+var expo = 123e+8;
+document.writeln(expo)
